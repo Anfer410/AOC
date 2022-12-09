d24aee797e2d7eed9e8ef336b652f542735d8c3a
Anfer <masterofevil410@gmail.com>
2022-12-08 22:54:31 -0500

    2022 day 8 python

--- 2022/python/day_8/main.py
+++ 2022/python/day_8/main.py
@@ -56,29 +56,104 @@ def look_at_trees(forest):
     return visible
 
 
+def find_neighbours(arr, trees):
+
+    potentials = {}
+    for tree_pos in trees:
+        potentials.update({tree_pos:{}})
+        
+        # look up 
+        view = []
+        tall_tree = arr[tree_pos[0]][tree_pos[1]]
+        
+        for i in reversed(range(0, tree_pos[0])):
+            if arr[i][tree_pos[1]] < tall_tree or arr[i][tree_pos[1]] > tall_tree:
+                view.append(arr[i][tree_pos[1]])
+                if arr[i][tree_pos[1]] >= tall_tree:
+                    tall_tree = arr[i][tree_pos[1]]
+            elif arr[i][tree_pos[1]] == tall_tree:
+                view.append(arr[i][tree_pos[1]])
+                break
+        
+        potentials[(tree_pos)].update({'up':view})
+        
+        # look down
+        view = []
+        tall_tree = arr[tree_pos[0]][tree_pos[1]]
+        for i in range(tree_pos[0]+1, len(arr)):
+            if arr[i][tree_pos[1]] < tall_tree or arr[i][tree_pos[1]] > tall_tree:
+                view.append(arr[i][tree_pos[1]])
+                if arr[i][tree_pos[1]] >= tall_tree:
+                    tall_tree = arr[i][tree_pos[1]]
+            elif arr[i][tree_pos[1]] == tall_tree:
+                view.append(arr[i][tree_pos[1]])
+                break
+        potentials[tree_pos].update({'down':view})
+
+        # look right
+        view = []
+        tall_tree = arr[tree_pos[0]][tree_pos[1]]
+        for i in range(tree_pos[1]+1,len(arr[tree_pos[0]])):
+            
+            if arr[tree_pos[0]][i] < tall_tree or arr[tree_pos[0]][i] > tall_tree:
+                view.append(arr[tree_pos[0]][i])
+                if arr[tree_pos[0]][i] >= tall_tree:
+                    tall_tree = arr[tree_pos[0]][i]
+            elif arr[tree_pos[0]][i] == tall_tree:
+                view.append(arr[tree_pos[0]][i])
+                break
+        
+        potentials[tree_pos].update({'right':view})
+
+        # look left
+        view = []
+        tall_tree = arr[tree_pos[0]][tree_pos[1]]
+        for i in reversed(range(0, tree_pos[1])):
+            if arr[tree_pos[0]][i] < tall_tree or arr[tree_pos[0]][i] > tall_tree:
+                view.append(arr[tree_pos[0]][i])
+                if arr[tree_pos[0]][i] >= tall_tree:
+                    tall_tree = arr[tree_pos[0]][i]
+            elif arr[tree_pos[0]][i] == tall_tree:
+                view.append(arr[tree_pos[0]][i])
+                break
+        
+        potentials[tree_pos].update({'left':view})
+            
+    return potentials
+
+
 def main():
-    input = read_input().splitlines()
     # input = test_input.splitlines()
+    input = read_input().splitlines()
     
     forest = []
     
     for line in input:
         forest.append([int(value) for value in line.replace('\n','')])
     
-    neighbours = look_at_trees(forest)
+    visible_trees = look_at_trees(forest)
     
-    for i in range(len(forest)):
-        char = ''
-        for j in range(len(forest)):
-            if (i,j) in neighbours or i == 0 or j==0 or i == len(forest)-1 or j==len(forest[i])-1:
-                char += str(forest[i][j])
-            else:
-                char += '#'
-        print(char)
+    # for i in range(len(forest)):
+    #     char = ''
+    #     for j in range(len(forest)):
+    #         if (i,j) in visible_trees or i == 0 or j==0 or i == len(forest)-1 or j==len(forest[i])-1:
+    #             char += str(forest[i][j])
+    #         else:
+    #             char += '#'
+    #     print(char)
     
     size = len(forest[0])*2 + len(forest) *2 - 4
-    print(f'Part 1: {size + len(neighbours)}')
+    print(f'Part 1: {size + len(visible_trees)}')
 
+    pot_spots = find_neighbours(forest, visible_trees)
+    
+    weight = []
+
+    for spot in pot_spots:
+        weight.append(len(pot_spots[spot]['left']) * len(pot_spots[spot]['up']) * len(pot_spots[spot]['down']) * len(pot_spots[spot]['right']))
+        
+    # print(weight)
+    print(f'Part 2: {sorted(weight, reverse=True)[0]}')
 
 if __name__ == "__main__":
     main()
